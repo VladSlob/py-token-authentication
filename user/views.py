20308fe44cdcf5cadf2e04d88eeee1bf4728e360
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-23 09:17:52 +0300

    views.py

--- user/views.py
+++ user/views.py
@@ -1,26 +1,24 @@
+from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
+
 from user.serializers import UserSerializer
-from rest_framework import generics, status
-from user.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
-class UserCreateView(generics.CreateAPIView):
+class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
 
 
-class LoginView(ObtainAuthToken):
+class LoginUserView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class UserUpdateView(generics.RetrieveUpdateAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
